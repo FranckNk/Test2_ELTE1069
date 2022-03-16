92980edbc504e56c6cdb3261ff308e40ace2838b
FranckNk <nkeuboufranck@gmail.com>
2022-03-16 10:52:01 -0300

    first commit

---
+++ include/Secrets.h
@@ -0,0 +1,32 @@
+/*
+SSID du WIFI ainsi que le mot de passe
+Modifier les paramètre WIFI pour le réseau auquels votre objets doit se brancher
+*/
+
+/*
+#define WIFI_SECRET_SSID "IOT_OBJECTS"
+#define WIFI_SECRET_PASS "42Bidules!"
+
+*/
+
+#define WIFI_SECRET_SSID "MotorolaGPower"
+#define WIFI_SECRET_PASS "Detergent!"
+
+/*
+Information nécessaire pour le branchement a un Broker MQTT
+Dans le cadre du cours AIIA1013, ne modifié pas cet information
+*/
+
+#define SECRET_MQTT_SERVER_IP "198.164.130.74"
+#define SECRET_MQTT_SERVER_PORT 1883
+
+/*
+Détails sur l'indentification de l'objet
+Cette information provient de l'objet virtuel créer sur le serveur Thingsbord
+*/
+
+#define SECRET_TOKEN  "DEazyRcRxYPWYGb9aLhp"
+#define SECRET_DEVICE_ID "570fba90-a52e-11ec-9235-d318ce92df6e"
+
+
+
---
+++ src/main.cpp
@@ -0,0 +1,65 @@
+/*
+
+TITRE          : Control Servo Moteur avec Detecteur de Rotation. Test2 de ELTE1069
+AUTEUR         : Franck Nkeubou Awougang
+DATE           : 09/02/2022
+DESCRIPTION    : Programme pour le Test de ELTE permettant de controler un système de fuite
+					d'eau dans un sous-sol avec un capteur et un relai pour actionner la
+					pompe d'évacuation d'eau.
+VERSION        : 0.0.1
+
+*/
+#include <Arduino.h>
+#include "WIFIConnector_MKR1000.h"
+#include "MQTTConnector.h"
+#include "Timer.h"
+
+// Déclaration des constantes.
+const int PIN_RELAI = 2;
+const int PIN_SENSOR_SWITCH = 4;
+
+// Déclaration des variables.
+bool EtatPompe = false; // La pompe d'évacuation est fermée.
+bool EtatRelai = false;
+
+Timer DelaiEnvoi;
+unsigned long DelaiAttente = 5000;
+// Déclaration des fonctions.
+void ActivationPompe();
+
+void setup() {
+	//Initialisation du wifi.
+	wifiConnect();                  //Branchement au réseau WIFI
+	MQTTConnect();                  //Branchement au broker MQTT
+	// initialisation du moniteur serie.
+	Serial.begin(9600);
+	// configuration du mode des broches.
+	pinMode(PIN_RELAI, OUTPUT);
+	pinMode(PIN_SENSOR_SWITCH, INPUT);
+	attachInterrupt(digitalPinToInterrupt(PIN_SENSOR_SWITCH), ActivationPompe, CHANGE);
+	DelaiEnvoi.startTimer(DelaiAttente);
+}
+
+void loop() {
+	// put your main code here, to run repeatedly:
+	if (EtatPompe)
+	{
+		digitalWrite(PIN_RELAI, HIGH);	
+		EtatRelai = true;	
+	}
+	else{
+		digitalWrite(PIN_RELAI, LOW);
+		EtatRelai = false;
+	}
+	if (DelaiEnvoi.isTimerReady())
+	{
+		appendPayload("EtatPompe", EtatPompe);
+		appendPayload("EtatRelai", EtatRelai);
+		sendPayload();
+		DelaiEnvoi.startTimer(DelaiAttente);
+	}
+}
+
+void ActivationPompe(){
+	EtatPompe = !EtatPompe;
+}
